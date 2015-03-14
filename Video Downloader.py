01c6c2091ec60267236f1d867fb02cb0d7dd7af9
amirjaffari <jaffari.amir@gmail.com>
2015-03-13 21:28:47 -0400

    Update Video Downloader.py

--- Video Downloader.py
+++ Video Downloader.py
@@ -11,12 +11,14 @@ r = praw.Reddit(user_agent=user_agent)
 
 submissions = r.get_subreddit('').get_top(limit=12)
 
+path = " "
+
 urls = []
 names=[]
 
 def yt() :
     for x in submissions:
-        ydl_opts = {'outtmpl': "/Users/amirjaffari/Desktop/YouTube/"+x.title + '.%(ext)s'}
+        ydl_opts = {'outtmpl': path +x.title + '.%(ext)s'}
         urls.append(str(x.url))
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
            ydl.download([x.url, ])
